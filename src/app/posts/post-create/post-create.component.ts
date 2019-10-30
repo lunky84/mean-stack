5dbef57456351a266cbf79bcd4fdbc7796427daa
John Downie <john@bluestone360.co.uk>
2019-10-30 19:23:31 +0000

    added angular material design

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  enteredValue = "";
+  newPost = "No Content";
+
+  constructor() { }
+
+  onAddPost(postInput: HTMLTextAreaElement) {
+    // console.dir(postInput);
+    // this.newPost = postInput.value;
+    this.newPost = this.enteredValue;
+  }
+
+  ngOnInit() {
+  }
+
+}
