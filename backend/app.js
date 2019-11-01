b843b2894d12cdc185249d7f2ae708bc13ac1a0d
John Downie <john.downie@roweit.co.uk>
2019-11-01 17:21:28 +0000

    added mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const Post = require("./models/post");
+const mongoose = require("mongoose");
+
+
 
 const app = express();
+
+mongoose.connect("mongodb+srv://john:sZWN7ppjccxk2ZU@cluster0-sawjs.mongodb.net/test?retryWrites=true&w=majority")
+    .then(() => {
+        console.log('Connected to database!');
+    })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,7 +35,10 @@ app.use((req, res, next) => {
 
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
     console.log(post);
     res.status(201).json({
         message: 'Post added successfully'
