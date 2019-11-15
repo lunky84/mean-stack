c38c3143207d2bf6177180e1f94fdfd2ee5df26c
John Downie <john.downie@roweit.co.uk>
2019-11-15 15:23:54 +0000

    mongo db, add, retrieve, delete

--- backend/app.js
+++ backend/app.js
@@ -7,29 +7,35 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://john:sZWN7ppjccxk2ZU@cluster0-sawjs.mongodb.net/test?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://john:kSVQ8aYb9SAiDcS@cluster0-bmsqz.mongodb.net/node-angular?retryWrites=true&w=majority")
     .then(() => {
         console.log('Connected to database!');
+    })
+    .catch(() => {
+        console.log('Connection failed');
     })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
     res.setHeader(
         "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, DELETE, OPTIONS"
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     );
     next();
 });
 
 app.use((req, res, next) => {
-    console.log('Frist middleware')
+    console.log('First middleware')
     next();
 });
 
@@ -40,29 +46,32 @@ app.post("/api/posts", (req, res, next) => {
         content: req.body.content
     });
     console.log(post);
-    res.status(201).json({
-        message: 'Post added successfully'
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            postId: createdPost._id
+        });
     });
 });
 
-app.use("/api/posts", (req, res, next) => {
-    // res.send('Hello from express');
-    const posts = [
-    {
-        id: 'fasd34424',
-        title: 'First server-side post',
-        content: 'This is coming from the server'
-    },
-    {
-        id: 'sdfer3443',
-        title: 'Second server-side post',
-        content: 'This is coming from the server'
-    }
-    ];
-    res.status(200).json({
-        message: 'Posts fetched succesfully!',
-        posts: posts
+app.get("/api/posts", (req, res, next) => {
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: 'Posts fetched succesfully!',
+            posts: documents
+        });
     });
+});
+
+
+app.delete("/api/posts/:id", (req, res, next) => {
+    console.log(req.params.id);
+    Post.deleteOne({_id: req.params.id}).then(
+        result => {
+            console.log(result);
+            res.status(200).json({ message:  "Post deleted!" });
+        }
+    )
 });
 
 module.exports = app;
